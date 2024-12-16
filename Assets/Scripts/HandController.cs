d4c7093f22f7cc371ce557e58afe8576ccf55a89
BWayRuby <150455155+BWayRuby@users.noreply.github.com>
2024-12-16 20:14:00 +0000

    Somewhat working hands
    -Replaced balls with hand model
    -Changed the child structure of rigged hands to work with code
    -Need to add Inverse Kinematics

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -24,7 +24,7 @@ public class HandController : MonoBehaviour
     public void UpdateHandPositions()
     {
         // iterating through the stored positions to update the in-game transforms
-        for (int i = 0; i < 21; i++) 
+        for (int i = 0; i < 23; i++) 
         {
             // getting points from api by index
             Vector3 rpoint = HandTrackingAPI.GetLandmark(i, HandType.Right);
