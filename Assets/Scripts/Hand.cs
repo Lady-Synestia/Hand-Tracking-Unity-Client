bf5814bcaf0ca911032268df10f884b13b985576
hellomeister3 <chloeeejohnson@outlook.com>
2024-12-19 17:04:09 +0000

    reverted to working visualisation
    also fixed point scaling

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -4,24 +4,22 @@ public class Hand : MonoBehaviour
 {
     [SerializeField] Transform[] childTransforms;
 
-    // Multiplier for raw position data
-    // stops points from being so clustered
-    // TODO: fix offset caused by multiplier
-    const int distanceMultiplier = -1;
+    // scalar for raw position data
+    Vector3 scalar = new Vector3(1, -1, 1);
 
     private void Start()
     {
         // number of children is expected to be 21
         // storing transforms of children so they don't have to be found again later
-        //childTransforms = new Transform[this.transform.childCount];
-        //for (int i = 0; i < this.transform.childCount; i++)
-        //{
-        //    childTransforms[i] = this.transform.GetChild(i);
-        //}
+        childTransforms = new Transform[this.transform.childCount];
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            childTransforms[i] = this.transform.GetChild(i);
+        }
     }
 
     public void SetChildPosition(int childIndex, Vector3 position)
     {
-        childTransforms[childIndex].position = position * distanceMultiplier;
+        childTransforms[childIndex].position = Vector3.Scale(position, scalar);
     }
 }
